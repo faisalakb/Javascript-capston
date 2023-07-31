9b4c5c8d2436da2fd4ef155d2f3921b016d43e7a
GhostEsso <lilipitaham@gmail.com>
2023-07-31 18:09:21 +0000

    header and footer section added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,1 @@
-import './style.css';
-import addItem from '../modules/addItem.js';
-import del from '../modules/deleteItem.js';
-import display from '../modules/display.js';
-
-const todoList = [];
-addItem(todoList);
-const clsBtn = document.getElementById('clsBtn');
-clsBtn.addEventListener('click', () => {
-  del(todoList);
-});
-
-display();
+import '../src/style.css';
