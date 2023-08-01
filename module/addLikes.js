a130f26f93696564e4047a17aace6ca8aaf39068
GhostEsso <lilipitaham@gmail.com>
2023-08-01 17:29:53 +0000

    addLikes.js modified to let display content on home page

--- module/addLikes.js
+++ module/addLikes.js
@@ -1,18 +1,25 @@
-async function addLikes(itemId) {
+// addLikes.js
+const addLikes = async (itemId) => {
   try {
-    await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/fy6DJ86u1eQhR7jHzpFg/likes/', {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify({
-        item_id: itemId,
-      }),
-    });
-    throw new Error('Like ajouté avec succès !');
+    const response = await fetch(
+      'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/fy6DJ86u1eQhR7jHzpFg/likes/',
+      {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          item_id: itemId,
+        }),
+      }
+    );
+
+    if (response.status !== 201) {
+      throw new Error('Erreur lors de la requête API.');
+    }
   } catch (error) {
-    throw new Error('Erreur lors de la requête API.', error);
+    console.error('Erreur lors de la requête API.', error);
   }
-}
+};
 
 export default addLikes;
