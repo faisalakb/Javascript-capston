1bd9541681c9ccb2a79651e2e3463e78cedf2a15
faisalakb <faisalakb786@gmail.com>
2023-08-02 13:53:51 +0500

    updated

--- module/enterComment.js
+++ module/enterComment.js
@@ -17,7 +17,7 @@ const addComments = async () => {
         throw new Error('Error entering like');
       }
 
-      return response.text(); // This returns a Promise that resolves with the JSON data
+      return response.text(); 
     })
     .catch((error) => {
       console.error('Like Error:', error);
