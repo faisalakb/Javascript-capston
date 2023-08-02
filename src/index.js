213c00cd84b9fd5b81a953509e5b50f3e4fbe8ec
faisalakb <faisalakb786@gmail.com>
2023-08-02 13:40:11 +0500

    Updated index.js file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import './style.css';
 import display from '../module/display.js';
-import getComments from '../module/getComment.js';
-import enterComment from '../module/enterComment.js';
 
-enterComment();
-getComments(1);
 display();
